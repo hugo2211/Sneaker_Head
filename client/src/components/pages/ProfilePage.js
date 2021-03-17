aeab91b024bfbe078ae52858de4238f06bc70352
Harrison Glover <harrison_glover@yahoo.com>
2021-03-16 21:03:41 -0500

    added logic for profile page

--- client/src/components/pages/ProfilePage.js
+++ client/src/components/pages/ProfilePage.js
@@ -5,7 +5,7 @@ import Header from "../general/Header";
 const ProfilePage = ({ history }) => {
   const [error, setError] = useState("");
   const [userInfo, setUserInfo] = useState("");
-
+  const [userShoeCollection, setUserShoeCollecton] = useState([]);
   const fetchUserData = async () => {
     const config = {
       headers: {
@@ -25,9 +25,25 @@ const ProfilePage = ({ history }) => {
       setError("You are not authorized please login");
     }
   };
+  const getShoes = () => {
+  
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${localStorage.getItem("authToken")}`,
+      },
+    };
+
+    axios.get('/api/private/shoes', config).then(function (response) {
+      console.log('shoes form backedn!!', response)
+      setUserShoeCollecton(response.data.data)
+
+    })
+  }
 
   useEffect(() => {
     fetchUserData();
+    getShoes();
   }, []);
 
   return (
@@ -37,6 +53,45 @@ const ProfilePage = ({ history }) => {
         <h2 className="text-center">Profile Page</h2>
         {error && <span className="error-message">{error}</span>}
       </div>
+
+      <div id="carouselExampleIndicators" className="carousel slide align-self-center" data-ride="carousel">
+  
+          <div className="carousel-inner">
+  
+            {userShoeCollection.map((shoe, i) => {
+  
+              var active = i === 0 ? 'carousel-item active' : 'carousel-item';
+              return (<div className={active}>
+                <img className="d-block w-50"
+                  src={shoe.image}
+                  style={{ height:'50px' }}
+                  alt="Coding Quiz Screenshot"></img>
+  
+                <a href="https://glove1911.github.io/Code-Quiz-Assignment/">
+                 
+                </a>
+  
+                <a href="https://github.com/Glove1911/Code-Quiz-Assignment">
+                  {/* <button type="button" className="btn btn-outline-primary"
+                    style={styles.button40Style}>Github Repository</button> */}
+                </a>
+  
+              </div>)
+            })}
+  
+  
+          </div>
+          <a className="carousel-control-prev " href="#carouselExampleIndicators" role="button"
+            data-slide="prev">
+            <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span className="sr-only">Previous</span>
+          </a>
+          <a className="carousel-control-next" href="#carouselExampleIndicators" role="button"
+            data-slide="next">
+            <span className="carousel-control-next-icon" aria-hidden="true"></span>
+            <span className="sr-only">Next</span>
+          </a>
+        </div>
     </div>
   );
 };
--- routes/private.js
+++ routes/private.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const { protect } = require("../middleware/auth");
-const { getChatProjectId } = require("../controllers/private");
+const { getChatProjectId, getUserShoes } = require("../controllers/private");
 
 router.route("/").get(protect, getChatProjectId); 
+router.route("/shoes").get(protect, getUserShoes); 
 
 module.exports = router;
