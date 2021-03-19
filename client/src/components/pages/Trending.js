c2403e867a6ec326dc96eeaa53c83584597e3930
Victor Mendizabal <2211hugo@gmail.com>
2021-03-18 20:17:20 -0500

    added card to trending page

--- client/src/components/pages/Trending.js
+++ client/src/components/pages/Trending.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Header from "../general/Header";
 import ShoeSquare from "../shoes/ShoeSquare";
+import ShoeCard from "../shoes/ShoeCard"
 import airJordan1MidHyperRoyal from "../../images/airJordan1MidHyperRoyal.jpg";
 import nikeDunkHighSpSyracuse from "../../images/nikeDunkHighSpSyracuse.webp";
 import "./Trending.css";
@@ -10,25 +11,66 @@ const Trending = ({ history }) => {
   return (
     <div className="trending-page">
       <Header history={history} />
-      <h2>Feed</h2>
+      <h1>Trending</h1>
+      <br/>
+      <h2>Shoe of the Day!</h2>
+      <div>
+        <ShoeCard userName="victor.mendizabal" image={airJordan1MidHyperRoyal}/>
+      </div>
+
+      <h3>What's Hot Right Now</h3>
       <div className="shoe-carosel">
         
-        <ShoeSquare image={airJordan1MidHyperRoyal} product="Jordan 1 Mid Hyper Royal" price="$132" />
-        <ShoeSquare image={airJordan1MidHyperRoyal} product="Jordan 1 Mid Hyper Royal" price="$132" />
-        <ShoeSquare image={airJordan1MidHyperRoyal} product="Jordan 1 Mid Hyper Royal" price="$132" />
-        <ShoeSquare image={airJordan1MidHyperRoyal} product="Jordan 1 Mid Hyper Royal" price="$132" />
-        <ShoeSquare image={airJordan1MidHyperRoyal} product="Jordan 1 Mid Hyper Royal" price="$132" />
-        <ShoeSquare image={airJordan1MidHyperRoyal} product="Jordan 1 Mid Hyper Royal" price="$132" />
-        <ShoeSquare image={airJordan1MidHyperRoyal} product="Jordan 1 Mid Hyper Royal" price="$132" />
-        <ShoeSquare image={airJordan1MidHyperRoyal} product="Jordan 1 Mid Hyper Royal" price="$132" />
-        <ShoeSquare image={airJordan1MidHyperRoyal} product="Jordan 1 Mid Hyper Royal" price="$132" />
-        <ShoeSquare image={airJordan1MidHyperRoyal} product="Jordan 1 Mid Hyper Royal" price="$132" />
-        <ShoeSquare image={airJordan1MidHyperRoyal} product="Jordan 1 Mid Hyper Royal" price="$132" />
-        <ShoeSquare image={airJordan1MidHyperRoyal} product="Jordan 1 Mid Hyper Royal" price="$132" />
-        <ShoeSquare image={airJordan1MidHyperRoyal} product="Jordan 1 Mid Hyper Royal" price="$132" />
-        <ShoeSquare image={airJordan1MidHyperRoyal} product="Jordan 1 Mid Hyper Royal" price="$132" />
-        <ShoeSquare image={airJordan1MidHyperRoyal} product="Jordan 1 Mid Hyper Royal" price="$132" />
-        <ShoeSquare image={airJordan1MidHyperRoyal} product="Jordan 1 Mid Hyper Royal" price="$132" />
+        <ShoeSquare userName="hugo2211" image={airJordan1MidHyperRoyal} product="Jordan 1 Mid Hyper Royal" price="$132" />
+        <ShoeSquare userName="anaBananaSplit" image={nikeDunkHighSpSyracuse} product="Nike Dunk /high Syracuse" price="$98" />
+        <ShoeSquare userName="Mark" image={airJordan1MidHyperRoyal} product="Jordan 1 Mid Hyper Royal" price="$132" />
+        <ShoeSquare userName="HarrysKicks" image={nikeDunkHighSpSyracuse} product="Nike Dunk /high Syracuse" price="$98" />
+        <ShoeSquare userName="BlockBetty" image={airJordan1MidHyperRoyal} product="Jordan 1 Mid Hyper Royal" price="$132" />
+        <ShoeSquare userName="KingDonut" image={nikeDunkHighSpSyracuse} product="Nike Dunk /high Syracuse" price="$98" />
+        <ShoeSquare userName="Victor.Mendizabal" image={airJordan1MidHyperRoyal} product="Jordan 1 Mid Hyper Royal" price="$132" />
+        <ShoeSquare userName="anaBananaSplit" image={nikeDunkHighSpSyracuse} product="Nike Dunk /high Syracuse" price="$98" />
+
+      </div>
+
+      <h3>New Releases</h3>
+      <div className="shoe-carosel">
+        
+      <ShoeSquare userName="hugo2211" image={airJordan1MidHyperRoyal} product="Jordan 1 Mid Hyper Royal" price="$132" />
+        <ShoeSquare userName="anaBananaSplit" image={nikeDunkHighSpSyracuse} product="Nike Dunk /high Syracuse" price="$98" />
+        <ShoeSquare userName="Mark" image={airJordan1MidHyperRoyal} product="Jordan 1 Mid Hyper Royal" price="$132" />
+        <ShoeSquare userName="HarrysKicks" image={nikeDunkHighSpSyracuse} product="Nike Dunk /high Syracuse" price="$98" />
+        <ShoeSquare userName="BlockBetty" image={airJordan1MidHyperRoyal} product="Jordan 1 Mid Hyper Royal" price="$132" />
+        <ShoeSquare userName="KingDonut" image={nikeDunkHighSpSyracuse} product="Nike Dunk /high Syracuse" price="$98" />
+        <ShoeSquare userName="Victor.Mendizabal" image={airJordan1MidHyperRoyal} product="Jordan 1 Mid Hyper Royal" price="$132" />
+        <ShoeSquare userName="anaBananaSplit" image={nikeDunkHighSpSyracuse} product="Nike Dunk /high Syracuse" price="$98" />
+
+      </div>
+
+      <h3>Old School</h3>
+      <div className="shoe-carosel">
+        
+      <ShoeSquare userName="hugo2211" image={airJordan1MidHyperRoyal} product="Jordan 1 Mid Hyper Royal" price="$132" />
+        <ShoeSquare userName="anaBananaSplit" image={nikeDunkHighSpSyracuse} product="Nike Dunk /high Syracuse" price="$98" />
+        <ShoeSquare userName="Mark" image={airJordan1MidHyperRoyal} product="Jordan 1 Mid Hyper Royal" price="$132" />
+        <ShoeSquare userName="HarrysKicks" image={nikeDunkHighSpSyracuse} product="Nike Dunk /high Syracuse" price="$98" />
+        <ShoeSquare userName="BlockBetty" image={airJordan1MidHyperRoyal} product="Jordan 1 Mid Hyper Royal" price="$132" />
+        <ShoeSquare userName="KingDonut" image={nikeDunkHighSpSyracuse} product="Nike Dunk /high Syracuse" price="$98" />
+        <ShoeSquare userName="Victor.Mendizabal" image={airJordan1MidHyperRoyal} product="Jordan 1 Mid Hyper Royal" price="$132" />
+        <ShoeSquare userName="anaBananaSplit" image={nikeDunkHighSpSyracuse} product="Nike Dunk /high Syracuse" price="$98" />
+
+      </div>
+
+      <h3>Classics</h3>
+      <div className="shoe-carosel">
+        
+      <ShoeSquare userName="hugo2211" image={airJordan1MidHyperRoyal} product="Jordan 1 Mid Hyper Royal" price="$132" />
+        <ShoeSquare userName="anaBananaSplit" image={nikeDunkHighSpSyracuse} product="Nike Dunk /high Syracuse" price="$98" />
+        <ShoeSquare userName="Mark" image={airJordan1MidHyperRoyal} product="Jordan 1 Mid Hyper Royal" price="$132" />
+        <ShoeSquare userName="HarrysKicks" image={nikeDunkHighSpSyracuse} product="Nike Dunk /high Syracuse" price="$98" />
+        <ShoeSquare userName="BlockBetty" image={airJordan1MidHyperRoyal} product="Jordan 1 Mid Hyper Royal" price="$132" />
+        <ShoeSquare userName="KingDonut" image={nikeDunkHighSpSyracuse} product="Nike Dunk /high Syracuse" price="$98" />
+        <ShoeSquare userName="Victor.Mendizabal" image={airJordan1MidHyperRoyal} product="Jordan 1 Mid Hyper Royal" price="$132" />
+        <ShoeSquare userName="anaBananaSplit" image={nikeDunkHighSpSyracuse} product="Nike Dunk /high Syracuse" price="$98" />
 
       </div>
     </div>
--- client/src/components/shoes/ShoeSquare.js
+++ client/src/components/shoes/ShoeSquare.js
@@ -1,10 +1,13 @@
 import React from "react";
+import LikeButton from "../inputs/Like";
 import "./ShoeSquare.css";
 
-const ShoeSquare = ({ product, price, image}) => {
+const ShoeSquare = ({ userName, product, price, image}) => {
   return (
     <div className="shoe-container">
+      <div> {userName} </div>
       <img className="feed-img" src={image} />
+      <div> <LikeButton/></div>
       <div className="mt-2">{product}</div>
       <div>{price}</div>
     </div>
