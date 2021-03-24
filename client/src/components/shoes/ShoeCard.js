a6f573e52833b58119c20a10882b0a000199c927
Mark Mulligan <mark.mulligan.jr1@gmail.com>
2021-03-23 21:57:10 -0500

    added victors stuff

--- client/src/components/shoes/ShoeCard.js
+++ client/src/components/shoes/ShoeCard.js
@@ -1,17 +1,17 @@
 import React from "react";
+import LikeButton from "../buttons/Like";
+import CommentBox from "../comment/CommentBox";
 import "./ShoeCard.css";
-
 const ShoeCard = ({ userName, product, price, image}) => {
     return (
       <div className="card text-center border m-auto" style={{width: "50%"}}>
         <h5 className="card-header">{userName}</h5>
         <img className="feed-img m-auto img-fluid" src={image}/>
-        <div> </div>
+        <div> <LikeButton/> </div>
         <div className="mt-2">{product}</div>
         <div>{price}</div>
-        <div></div>
+        <div><CommentBox/></div>
       </div>
     );
   };
-  
   export default ShoeCard;
