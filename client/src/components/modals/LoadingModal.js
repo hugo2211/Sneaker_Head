91f6830ec3b688d9c5f970d74cb7931bd7686dc9
Mark Mulligan <mark.mulligan.jr1@gmail.com>
2021-03-31 23:47:31 -0500

    minor fiexes

--- client/src/components/modals/LoadingModal.js
+++ client/src/components/modals/LoadingModal.js
@@ -1,21 +1,9 @@
 import React, { useState } from "react";
 import { Modal } from "react-bootstrap";
-import { makeStyles } from "@material-ui/core/styles";
 import CircularProgress from "@material-ui/core/CircularProgress";
 import './Modals.css';
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    display: "flex",
-    "& > * + *": {
-      marginLeft: theme.spacing(2),
-    },
-  },
-}));
-
 const LoadingModal = ({ modalMessage, showLoadingScreen }) => {
-  const classes = useStyles();
-
   return (
     <>
       <Modal show={showLoadingScreen} backdrop="static" keyboard={false} className="my-modal">
